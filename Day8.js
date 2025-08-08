9ecc8879eb0991d5488c9590916afd957a3546fb
chanthramugei <chanthramugeiboopathi@gmail.com>
2025-08-08 15:36:19 +0530

    Update Day8.js

--- Day8.js
+++ Day8.js
@@ -1,9 +1,11 @@
+//current date and time
 let current=new Date("2024-08-08");
 let day=current.getDay();
 console.log(current)//unt
 console.log(current.toString());//local
 console.log(day);
 
+//constructor
 class Sample{
     constructor(personname){
         this.personname=personnmae;
@@ -12,6 +14,7 @@ class Sample{
 }
 let result=new Sample("Taehyung");
 
+//class
 class Bank{
     constructor(initialamount=0){
         this._balance=initialamount;
@@ -28,4 +31,7 @@ class Bank{
     }
 }
 let b=new Bank(10);
+console.log(b);
+
+b._balance=100;
 console.log(b);
